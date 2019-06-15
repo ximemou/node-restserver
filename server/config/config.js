44ae5f404399f720b7926d2675730c4297bd4a3e
ximena moure <xime_mou@hotmail.com>
2019-06-15 16:18:13 -0300

    widget

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.CADUCIDAD_TOKEN = 60*60*24*30;
 
 
+//mercado libre
+
+
+
+
+
 //Seed de autenticacion
 
 process.env.SEED =process.env.SEED ||'este-es-el-seed-desarrollo';
--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 require('./config/config');
+
+
+//MELI.init({ client_id: process.env.MERCADO_CLIENT});
+
 const express = require('express');
 // Using Node.js `require()`
 const mongoose = require('mongoose');
@@ -29,3 +33,50 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: tru
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto: ', 3000);
 });
+
+// PRUEBA MERCADO LIBRE
+
+var MercadoLibreStrategy = require('passport-mercadolibre').Strategy;
+
+passport.use(new MercadoLibreStrategy({
+    clientID: process.env.MERCADO_CLIENT,
+    clientSecret: process.env.SECRET_KEY,
+    callbackURL: process.env.CALLLBACK_URL,
+  },
+  function (accessToken, refreshToken, profile, done) {
+    // + store/retrieve user from database, together with access token and refresh token
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+app.get('/auth/mercadolibre',
+  passport.authorize('mercadolibre'));
+
+app.get('/auth/mercadolibre/callback',
+  passport.authorize('mercadolibre', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+app.get('/', ensureAuthenticated,
+  function(req, res) {
+    res.send("Logged in user: " + req.user.nickname);
+  }
+);
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  };
+  res.redirect('/auth/mercadolibre');
+};
