c34db843bbd01907ee1d334b930c7d604d16460b
ximena moure <xime_mou@hotmail.com>
2019-07-05 14:46:40 -0300

    widget

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(require('./routes/index'));
 
 
 
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true } ,(err, res) => {
+mongoose.connect(process.env.URLDB, (err, res) => {
 
     if (err) throw err;
 
